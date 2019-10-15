bba294c36477ede692f12adf50227c3cbb478bfa
Rushang <rushangsborda@gmail.com>
2019-10-15 20:56:41 +0530

    update and delete

--- routes/rough.js
+++ routes/rough.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Post = require('../models/Rough');
 
 router.get('/', async(req,res)=> {
-    const data = await Post.find();
-    const data_carat = await Post.find({carat : req.body.carat,completed : "false"});
+    const data = await Post.find({completed : "false"});
+    //const data_carat = await Post.find({carat : req.body.carat,completed : "false"});
     //console.log(data_carat);
     res.json(data.reverse());
 })
@@ -30,5 +30,58 @@ router.post('/', async(req,res) => {
         res.json({message : error});
     }
 });
+
+
+router.patch('/:id', async (req,res) => {
+
+    const data = await Post.findOne({_id : req.params.id});
+   // console.log(data);
+    var name =req.body.name || data.name,
+    co = req.body.completed || data.completed,
+    carat = req.body.carat || data.carat,
+    price = req.body.price || data.price;
+    
+
+    try {
+
+
+        const updatePost = await Post.updateOne(
+            {_id : req.params.id},
+            {$set :{name : name,
+                completed : co,
+                carat : carat,
+                price : price,
+                rough_total : carat * price
+        }}
+        );
+       //res.json(updatePost);
+           if(updatePost != null){
+            res.json({message : 'Data Updated Successfully'});
+        }   
+        else{
+            res.json({message : 'Database Error'});
+        }       
+    } catch (error) {
+        res.json({message : error});
+    }
+
+
+
+
+});
+
+
+router.delete('/:id',async(req,res)=> {
+    try {
+        const removePost = await Post.deleteOne({_id : req.params.id});
+        res.json({message : "Deleted Successfully"});        
+    } catch (error) {
+        res.json(error);
+    }
+    
+});
+
+
+
 
 module.exports = router;
--- routes/sorting.js
+++ routes/sorting.js
@@ -29,10 +29,10 @@ router.get('/:id',async(req,res)=> {
 
 })
 
-router.get('/rough',async(req,res)=> {
+router.get('/',async(req,res)=> {
     try {
-        const post = await rough.find();                
-        res.json(post);
+        const post = await Post.find();                
+        res.json(post.reverse());
     } catch (error) {
         res.send({message : error});
     }
@@ -277,6 +277,118 @@ router.post('/', async(req,res) => {
     }
     
     
+
+});
+
+router.delete('/:id',async(req,res)=> {
+    try {
+        const removePost = await Post.deleteOne({rough_id : req.params.id});
+        res.json({message : "Deleted Successfully"});        
+    } catch (error) {
+        res.json(error);
+    }
+    
+});
+
+router.patch('/:id',async (req,res) => {
+
+    var uprt,uppr;
+
+        try {
+            const post = await rough.findById(req.params.id);
+            uprt = post.carat;
+            uppr = post.rough_total;         
+        } catch (error) {
+            res.json({message : error})        
+        }
+
+      
+
+
+        const data_pre_sort = await Post.findOne({rough_id : req.params.id});
+        
+        const c_ca = Number(req.body.chocki_carat) || Number(data_pre_sort.chocki.chocki_carat), 
+        c_pr = Number(req.body.chocki_price) || Number(data_pre_sort.chocki.chocki_price),
+        o_ca = Number(req.body.out_carat) ||  Number(data_pre_sort.out.out_carat),
+        o_pr = Number(req.body.out_price) || Number(data_pre_sort.out.out_price), 
+        m_ca = Number(req.body.markis_carat) ||  Number(data_pre_sort.markis.markis_carat), 
+        m_pr = Number(req.body.markis_price) || Number(data_pre_sort.markis.markis_price),
+        g_ca = Number(req.body.gol_carat) || Number(data_pre_sort.gol.gol_carat),
+        g_pr = Number(req.body.gol_price) || Number(data_pre_sort.gol.gol_price),
+        cr_ca = Number(req.body.crystal_carat) || Number(data_pre_sort.crystal.crystal_carat),
+        cr_pr = Number(req.body.crystal_price) || Number(data_pre_sort.crystal.crystal_price),
+        c_tl = c_pr * c_ca , 
+        o_tl = o_ca * o_pr,
+        m_tl =m_ca * m_pr ,
+        g_tl = g_ca * g_pr , 
+        cr_tl = cr_pr * cr_ca;
+
+
+
+
+    try {
+
+
+        const updatePost = await Post.updateOne(
+            {rough_id : req.params.id},
+            {$set :{chocki : {
+                chocki_carat : c_ca,
+                chocki_price : c_pr,
+                chocki_total : c_tl
+            },
+            out : {
+                out_carat : o_ca,
+                out_price : o_pr,
+                out_total : o_tl
+            },
+            markis : {
+                markis_carat : m_ca,
+                markis_price : m_pr,
+                markis_total : m_tl
+            },
+            gol : {
+                gol_carat : g_ca,
+                gol_price : g_pr,
+                gol_total : g_tl
+            },
+            crystal : {
+                crystal_carat : cr_ca,
+                crystal_price : cr_pr,
+                crystal_total : cr_tl
+            },
+            sort_total_carat : c_ca + o_ca + m_ca + g_ca + cr_ca,
+            sort_total_price : c_tl + o_tl + m_tl + g_tl + cr_tl, 
+            updated_rough_carat : uprt - (c_ca + o_ca + m_ca + g_ca + cr_ca),
+            updated_rough_price : uppr - (c_tl + o_tl + m_tl + g_tl + cr_tl)
+        }}
+        );
+       //res.json(updatePost);
+           if(updatePost != null){
+            res.json({message : 'Data Updated Successfully'});
+        }   
+        else{
+            res.json({message : 'Database Error'});
+        }       
+    } catch (error) {
+        res.json({message : error});
+    }
+
+    // try {
+    //     await Unused.updateOne(
+    //         {rough_id : req.params.id},
+    //         {$set :{
+    //             carat : uprt,
+    //             unused_carat : uprt - (uc_ca + uo_ca + um_ca + ug_ca + ucr_ca),
+    //             unused_total : uppr - (uc_tl + uo_tl + um_tl + ug_tl + ucr_tl)
+    //         }}
+    //     );
+    // } catch (error) {
+    //     res.json({message : error});            
+    // }
+
+
+
+
 
 });
 
